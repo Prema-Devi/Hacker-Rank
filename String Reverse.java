4b7f5f19527891c68e4ffe325f500835dfc8b4dd
Prema-Devi <kuppus002_bit27@mepcoeng.ac.in>
2026-03-27 10:31:47 +0530

    Create String Reverse.java

---
+++ String Reverse.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String res = "Yes";
+        for(int i = 0;i< A.length()/2;i++){
+            if(A.charAt(i) != A.charAt(A.length()-1-i)){
+              res = "No";  
+            }
+        }
+        System.out.println(res);
+    }
+}
+
+
+
+
